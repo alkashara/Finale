89d21522bf096596cf46f6d892dafd015bd61907
alkashara <tyrinok888@gmail.com>
2024-05-20 16:22:38 +0300

    Updated start.sh and other scripts for proper PostgreSQL replication setup

--- db/init-pg-hba.sh
+++ db/init-pg-hba.sh
@@ -6,6 +6,8 @@ echo "host all all 0.0.0.0/0 scram-sha-256" >> /var/lib/postgresql/data/pg_hba.c
 echo "host replication replicator 0.0.0.0/0 scram-sha-256" >> /var/lib/postgresql/data/pg_hba.conf
 echo "host all all 0.0.0.0/0 md5" >> /var/lib/postgresql/data/pg_hba.conf
 echo "local all all peer" >> /var/lib/postgresql/data/pg_hba.conf
+echo "host replication replicator 0.0.0.0/0 md5" >> /var/lib/postgresql/data/pg_hba.conf
+
 
 chmod 600 /var/lib/postgresql/data/pg_hba.conf
 
